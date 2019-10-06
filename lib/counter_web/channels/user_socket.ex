657b60bdca05744767bbdab25df8b10e5400f9b1
Neil Lyons <nwjlyons@googlemail.com>
2019-10-06 21:41:51 +0100

    Fix Heroku H15 Idle Connectoin errors

--- lib/counter_web/channels/user_socket.ex
+++ lib/counter_web/channels/user_socket.ex
@@ -4,6 +4,10 @@ defmodule CounterWeb.UserSocket do
   ## Channels
   # channel "room:*", CounterWeb.RoomChannel
 
+  ## Transports
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
+
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
   # verification, you can put default assigns into
