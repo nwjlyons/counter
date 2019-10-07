bfdc12b29c72f93b2d0d26a674ce56d3a6905c9e
Neil Lyons <nwjlyons@googlemail.com>
2019-10-07 20:53:42 +0100

    Disable check origin for now just to get live view working

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :counter, CounterWeb.Endpoint,
   url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info
