73185fc85c0fd2b34cf5202302139ca4b62cd280
Neil Lyons <nwjlyons@googlemail.com>
2019-10-09 18:00:17 +0100

    Experiement with autocomplete

--- lib/counter_web/live/page_live/index.ex
+++ lib/counter_web/live/page_live/index.ex
@@ -2,7 +2,7 @@ defmodule CounterWeb.PageLive.Index do
   use Phoenix.LiveView
 
   def mount(_session, socket) do
-    {:ok, assign(socket, count: 0)}
+    {:ok, assign(socket, count: 0, items: [])}
   end
 
   def render(assigns) do
@@ -19,5 +19,19 @@ defmodule CounterWeb.PageLive.Index do
 
   def handle_event("reset", _, socket) do
     {:noreply, assign(socket, count: 0)}
+  end
+
+  def handle_event("activateSearch", _, socket) do
+    {:noreply, assign(socket, count: 0, items: ["Apples", "Oranges", "Strawberries"])}
+  end
+
+  def handle_event("deactivateSearch", _, socket) do
+    {:noreply, assign(socket, items: [])}
+  end
+
+  def handle_event("filterSearch", %{"value" => value}, socket) do
+    items = ["Apples", "Oranges", "Strawberries"]
+    itemsFiltered = Enum.filter(items, fn(item) -> String.starts_with?((String.downcase item), (String.downcase value)) end)
+    {:noreply, assign(socket, items: itemsFiltered)}
   end
 end
